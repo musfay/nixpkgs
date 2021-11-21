f7deb2c812ba23cde72ea46eafb6fc6beadca73d
Mustafa Çalışkan <musfay@protonmail.com>
2021-11-21 09:55:35 +0300

    bootiso: make bootiso find syslinux

--- pkgs/tools/cd-dvd/bootiso/default.nix
+++ pkgs/tools/cd-dvd/bootiso/default.nix
@@ -25,6 +25,8 @@ stdenvNoCC.mkDerivation rec {
     sha256 = "1l09d543b73r0wbpsj5m6kski8nq48lbraq1myxhidkgl3mm3d5i";
   };
 
+  patches = [ ./syslinux-lib.patch ];
+
   strictDeps = true;
   buildInputs = [ bash ];
   nativeBuildInputs = [ makeWrapper ];
